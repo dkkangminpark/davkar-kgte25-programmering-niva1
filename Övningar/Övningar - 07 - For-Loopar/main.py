66d75bff4c75af2fd5eb5fac337db60dc7c68d26
Davenk <123252013+TheDavenk@users.noreply.github.com>
2026-04-09 14:50:43 +0200

    Fortsatte med for-loopar (övning 5-7)

--- Övningar/Övningar - 07 - For-Loopar/main.py
+++ Övningar/Övningar - 07 - For-Loopar/main.py
@@ -23,9 +23,37 @@ time.sleep(1)
 print("\n4. Multiplikationstabell för 7")
 for x in range(1,11): print(x*7)
 
-time.sleep(1)"""
+time.sleep(1)
 
 print("\n5. Summan av talen 1-100")
 sumOfNumbers = 0
 for x in range (1,101): sumOfNumbers += x
 print(sumOfNumbers)
+
+time.sleep(1)
+
+print("\n6. Upp till n")
+ans = int(input("Skriv ett tal: "))
+for x in range(1, ans+1): print(x)
+
+time.sleep(1)
+
+print("\n7. Skriva ut bokstäver med .join()")
+mening = input("Skriv en mening: ")
+print("*".join(mening))
+
+time.sleep(1)
+
+print("\n7. Skriva ut bokstäver med for-loop")
+mening = input("Skriv en mening: ")
+resultat = ""
+
+for tecken in mening:
+    resultat += tecken + "*"
+
+# Ta bort sista *
+resultat = resultat[:-1]
+
+print(resultat)
+
+time.sleep(1)"""

824cafa6b1f8a26793bbc36a885c3f23754c715d
David Karlsson <davkar0926@edu.halmstad.se>
2026-09-09 09:21:47 +0200

    Fortsatte med listor

--- Övningar/Övningar - 10 - Listor/main.py
+++ Övningar/Övningar - 10 - Listor/main.py
@@ -22,3 +22,26 @@ na_li.append("Jeffrey Epstein")
 na_li.append("Donald J. Trump")
 for names in na_li:
     print(names)
+#end for
+
+time.sleep(1)
+
+print("\n3. Skapa en lista med fem tal. Skriv ut de första och sista talet med index.")
+three_li = [21, 54, 65, 98, 42]
+print(three_li[0], three_li[-1])
+
+time.sleep(1)
+
+print("\n4. Skapa en lista med fem färger")
+colors_list = ["Rosa", "Blå", "Röd", "Grön", "Brun"]
+colors_list[2] = "Lila"
+print(colors_list)
+
+time.sleep(1)
+
+print("\nNivå 2 - Indexering och Slicing")
+print("============\n")
+
+print("1. Skapa en lista med sju bokstäver. Skriv ut bokstävenra från index 2-5 med slicing")
+char_list = ["a", "b", "c", "d", "e", "f", "g"]
+print(char_list[2:6])

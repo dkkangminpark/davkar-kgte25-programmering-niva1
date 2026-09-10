3739947f88b5ee71e0958023e3fb5bfbcf9bed3c
David Karlsson <davkar0926@edu.halmstad.se>
2026-09-10 14:42:46 +0200

    Fortsatte med listor

--- Övningar/Övningar - 10 - Listor/main.py
+++ Övningar/Övningar - 10 - Listor/main.py
@@ -1,6 +1,6 @@
 import time
 
-print("\nÖvningar - 08 » While-loopar")
+print("\nÖvningar - 10 » Listor")
 print("=============================\n")
 
 print("\nNivå 1 - Grundläggande listor")
@@ -11,7 +11,7 @@ fruit_list = ["Banana", "Apple", "Pineapple"]
 
 for fruits in fruit_list:
     print(fruits)
-#end for
+# end for
 
 time.sleep(1)
 
@@ -22,7 +22,7 @@ na_li.append("Jeffrey Epstein")
 na_li.append("Donald J. Trump")
 for names in na_li:
     print(names)
-#end for
+# end for
 
 time.sleep(1)
 
@@ -45,3 +45,32 @@ print("============\n")
 print("1. Skapa en lista med sju bokstäver. Skriv ut bokstävenra från index 2-5 med slicing")
 char_list = ["a", "b", "c", "d", "e", "f", "g"]
 print(char_list[2:6])
+
+time.sleep(1)
+
+print("\n2. Skapa en lista med fem djur. Skriv ut det näst sista djuret med negativ indexering.")
+animal_list = ["Hund", "Katt", "Häst", "Kanin", "Lejon"]
+print(animal_list[-2])
+
+time.sleep(1)
+
+print("\n3. Skapa en lista med fem städer. Skriv ut de tre första med slicing")
+city_list = ["Stockholm", "Göteborg", "Malmö", "Halmstad", "Värö"]
+print(city_list[1:4])
+
+print("\n4. Skapa en lista med fem siffror. Skriv ut de alla utom den sista med slicing.")
+city_list = ["Stockholm", "Göteborg", "Malmö", "Halmstad", "Värö"]
+print(city_list[1:4])
+
+time.sleep(1)
+
+print("\nNivå 3 - Iteration & längd")
+print("============\n")
+
+print("1.Skapa en lista med fem maträtter. Skriv ut varje maträtt med en for-loop.")
+food_list = ["Pizza", "Tacos", "Sushi", "Köttbullar", "Lasagne"]
+for food in food_list:
+    print(food)
+#end for
+
+print("\n2. Skapa en lista med sju tal. Skriv ut hur många tal som finns med len().")
